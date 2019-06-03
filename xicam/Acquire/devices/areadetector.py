8b4c3ffbcefc0b9cb0becd627e31534d2ba958c7
Ronald Pandolfi <ronpandolfi@gmail.com>
2019-06-03 15:51:45 -0700

    Restore pool_max_buffers attribute

--- xicam/Acquire/devices/areadetector.py
+++ xicam/Acquire/devices/areadetector.py
@@ -1,18 +1,10 @@
-from ophyd import areadetector, PilatusDetector
-
-
-class SC(areadetector.cam.AreaDetectorCam):
-    pool_max_buffers = None
-
-
-class IP(areadetector.ImagePlugin):
-    pool_max_buffers = None
+from ophyd import areadetector
 
 
 class AreaDetector(areadetector.AreaDetector):
-    image1 = areadetector.Component(IP, 'image1:')
-    cam = areadetector.Component(SC, 'cam1:')
+    image1 = areadetector.Component(areadetector.ImagePlugin, 'image1:')
+    cam = areadetector.Component(areadetector.cam.AreaDetectorCam, 'cam1:')
 
 
 class PilatusDetector(areadetector.PilatusDetector):
-    image1 = areadetector.Component(IP, 'image1:')
+    image1 = areadetector.Component(areadetector.ImagePlugin, 'image1:')
