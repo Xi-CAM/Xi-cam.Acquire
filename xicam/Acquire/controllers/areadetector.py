a85013c3652b6a61ed5fcf339dfd7374f7931d87
ronpandolfi <ronpandolfi@gmail.com>
2020-03-04 15:31:31 -0800

    Better device threading; added display mixins

--- xicam/Acquire/controllers/areadetector.py
+++ xicam/Acquire/controllers/areadetector.py
@@ -9,19 +9,32 @@ from xicam.plugins import ControllerPlugin
 from functools import partial
 from xicam.core import msg
 from xicam.gui.widgets.dynimageview import DynImageView
+from xicam.gui.widgets.imageviewmixins import PixelCoordinates, Crosshair, BetterButtons, LogScaleIntensity, ImageViewHistogramOverflowFix
 from caproto._utils import CaprotoTimeoutError
+from ophyd.signal import ConnectionTimeoutError
 from pydm.widgets.line_edit import PyDMLineEdit
 from pydm.widgets.enum_combo_box import PyDMEnumComboBox
 from bluesky.plans import count
-
+from timeit import default_timer
+from contextlib import contextmanager
+# from xicam.SAXS.processing.correction import CorrectFastCCDImage
 from xicam.Acquire.runengine import RE
 import time
 
 
-class AreaDetectorController(ControllerPlugin):
-    viewclass = DynImageView
+class ADImageView(DynImageView,
+                  PixelCoordinates,
+                  Crosshair,
+                  BetterButtons,
+                  # LogScaleIntensity,
+                  ImageViewHistogramOverflowFix):
+    pass
 
-    def __init__(self, device, maxfps=30):
+
+class AreaDetectorController(ControllerPlugin):
+    viewclass = ADImageView
+
+    def __init__(self, device, maxfps=1):
         super(AreaDetectorController, self).__init__(device)
         self.maxfps = maxfps
         self._autolevel = True
@@ -31,7 +44,7 @@ class AreaDetectorController(ControllerPlugin):
         self.imageview = self.viewclass()
         self.passive = QCheckBox('Passive Mode')
         self.passive.setChecked(True)
-        self.error_text = pg.TextItem('')
+        self.error_text = pg.TextItem('Connecting to device...')
         self.imageview.view.addItem(self.error_text)
         self.layout().addWidget(self.imageview)
         self.layout().addWidget(self.passive)
@@ -60,11 +73,6 @@ class AreaDetectorController(ControllerPlugin):
         hlayout.addWidget(acquire_panel)
         self.layout().addLayout(hlayout)
 
-        self.thread = None
-        self.timer = QTimer()
-
-        partial(self.imageview.imageItem.setImage, autoLevels=False)
-
         # WIP
         # self.lutCheck = QCheckBox()
         # self.LUT = GradientWidget()
@@ -72,21 +80,40 @@ class AreaDetectorController(ControllerPlugin):
         # self.scaleCheck = QCheckBox()
         # self.rgbLevelsCheck = QCheckBox()
 
-        self.timer.singleShot(1. / self.maxfps * 1000, self.update)
         self._last_timestamp = time.time()
 
-    def update(self):
-        self.thread = threads.QThreadFuture(self.getFrame, showBusy=False,
-                                            callback_slot=partial(self.setFrame, autoLevels=self._autolevel),
-                                            except_slot=self.setError)
+        self.thread = threads.QThreadFutureIterator(self.update,
+                                                    showBusy=False,
+                                                    callback_slot=self.setFrame,
+                                                    except_slot=self.setError,
+                                                    threadkey=f'device-updater-{self.device.name}')
         self.thread.start()
+
+    def update(self):
+        with msg.busyContext():
+            msg.showMessage('Instantiating device...')
+            device = self.device.device_obj  # Force cache the device_obj
+
+            if not self.device.device_obj.trigger_staged:
+                msg.showMessage('Staging the device...')
+                self.device.device_obj.trigger()
+
+        msg.showMessage('Device ready!')
+
+        while True:
+            if not self.visibleRegion().isEmpty():
+                yield self.getFrame()
+            time.sleep(1./self.maxfps)
 
     def getFrame(self):
         try:
             if not self.passive.isChecked():
                 self.device.device_obj.trigger()
-            return self.device.device_obj.image1.shaped_image.get()
-        except (RuntimeError, CaprotoTimeoutError) as ex:
+            data = self.device.device_obj.image1.shaped_image.get()
+            # TODO: apply corrections to display; requires access to flats and darks
+            # data = np.squeeze(CorrectFastCCDImage().asfunction(images=data,)['corrected_images'].value)
+            return data
+        except (RuntimeError, CaprotoTimeoutError, ConnectionTimeoutError) as ex:
             msg.logError(ex)
         return None
 
@@ -97,18 +124,15 @@ class AreaDetectorController(ControllerPlugin):
             self.imageview.image = image
             # self.imageview.updateImage(autoHistogramRange=kwargs['autoLevels'])
             image = self.imageview.getProcessedImage()
-            if kwargs['autoLevels']:
+            if self._autolevel:
                 self.imageview.ui.histogram.setHistogramRange(self.imageview.levelMin, self.imageview.levelMax)
                 self.imageview.autoLevels()
             self.imageview.imageItem.updateImage(image)
 
-            # self.imageview.setImage(image, *args, **kwargs)
             self._autolevel = False
 
         self.error_text.setText(f'FPS: {1. / (time.time() - self._last_timestamp):.2f}')
         self._last_timestamp = time.time()
-
-        self.timer.singleShot(1. / self.maxfps * 1000, self.update)
 
     def setError(self, exception: Exception):
         msg.logError(exception)
