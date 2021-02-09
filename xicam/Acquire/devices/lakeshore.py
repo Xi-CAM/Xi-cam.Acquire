32313f8f2a478262eed64b237a25261a7b6712e1
Ronald Pandolfi <ronpandolfi@gmail.com>
2021-02-08 16:43:55 -0800

    add missing imports

--- xicam/Acquire/devices/lakeshore.py
+++ xicam/Acquire/devices/lakeshore.py
@@ -1,12 +1,11 @@
+from ophyd import Component as Cpt, Device
 from ophyd.signal import (EpicsSignalRO, EpicsSignal)
 from ophyd.areadetector.base import EpicsSignalWithRBV
-
 
 
 class LakeShore336(Device):
     temp_celsius = Cpt(EpicsSignalRO, 'TemperatureCelsius')
     temp_kelvin = Cpt(EpicsSignalRO, 'TemperatureKelvin')
     heater_output = Cpt(EpicsSignalRO, 'HeaterOutput')
-
     temp_limit = Cpt(EpicsSignalWithRBV, 'TemperatureLimit')
     temp_set_point = Cpt(EpicsSignalWithRBV, 'TemperatureSetPoint')
