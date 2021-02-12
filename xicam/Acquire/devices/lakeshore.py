e35c27cc04c7b2848e70b4e2537437a612ef2186
Juliane Reinhardt <jreinhardt@lbl.gov>
2021-02-12 13:18:35 -1000

    add doc to lakeshore ophyd device

--- xicam/Acquire/devices/lakeshore.py
+++ xicam/Acquire/devices/lakeshore.py
@@ -4,9 +4,17 @@ from ophyd.areadetector.base import EpicsSignalWithRBV
 
 
 class LakeShore336(Device):
+    """
+    Ophyd Device for the Lakeshore 336 temperature controller
+    https://www.lakeshore.com/docs/default-source/product-downloads/336_manual.pdf?sfvrsn=fa4e3a80_5
+    based on lakeshore336_ioc
+    https://github.com/lbl-camera/fastccd_support_ioc/blob/lakeshore336/fastccd_support_ioc/lakeshore336_ioc.py
+    """
+    # read-only components (PVs)
     temp_celsius = Cpt(EpicsSignalRO, 'TemperatureCelsius')
     temp_kelvin = Cpt(EpicsSignalRO, 'TemperatureKelvin')
     heater_output = Cpt(EpicsSignalRO, 'HeaterOutput')
 
+    # read-write components (PVs)
     temp_limit = Cpt(EpicsSignalWithRBV, 'TemperatureLimit')
     temp_set_point = Cpt(EpicsSignalWithRBV, 'TemperatureSetPoint')
