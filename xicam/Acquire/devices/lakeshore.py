536c454082c71623d00344fb3068fa061c4b03a8
Ronald Pandolfi <ronpandolfi@gmail.com>
2021-02-17 15:56:04 -0800

    Add units

--- xicam/Acquire/devices/lakeshore.py
+++ xicam/Acquire/devices/lakeshore.py
@@ -6,6 +6,6 @@ from ophyd.areadetector.base import EpicsSignalWithRBV
 class LakeShore336(Device):
     temp_celsius = Cpt(EpicsSignalRO, 'TemperatureCelsius', name='Temperature (°C)')
     temp_kelvin = Cpt(EpicsSignalRO, 'TemperatureKelvin', name='Temperature (°K)')
-    heater_output = Cpt(EpicsSignalRO, 'HeaterOutput', name='Heater Output')
+    heater_output = Cpt(EpicsSignalRO, 'HeaterOutput', name='Heater Output (%)')
     temp_limit = Cpt(EpicsSignalWithRBV, 'TemperatureLimit', name='Temperature Limit (°K)')
     temp_set_point = Cpt(EpicsSignalWithRBV, 'TemperatureSetPoint', name='Temperature Setpoint (°C)')
