b01745eeccd1d4102bcdd43565da34f18d0fdb20
Ronald Pandolfi <ronpandolfi@gmail.com>
2021-04-09 15:02:34 -0700

    Diode controller layout improvements

--- xicam/Acquire/controllers/diodecontroller.py
+++ xicam/Acquire/controllers/diodecontroller.py
@@ -9,8 +9,11 @@ from ophyd import Device
 from pydm.widgets.line_edit import PyDMLineEdit
 from pydm.widgets.enum_combo_box import PyDMEnumComboBox
 from pydm.widgets.pushbutton import PyDMPushButton
-from qtpy.QtWidgets import QWidget, QVBoxLayout, QCheckBox, QGroupBox, QFormLayout, QHBoxLayout, QPushButton, QComboBox, QApplication, QLineEdit
-
+from qtpy.QtWidgets import QWidget, QVBoxLayout, QCheckBox, QGroupBox, QFormLayout, QHBoxLayout, QPushButton, QComboBox, \
+    QApplication, QLineEdit, QSplitter
+from qtpy.QtCore import Qt
+from typhos import TyphosCompositeSignalPanel
+from pydm.widgets.timeplot import PyDMTimePlot
 from xicam.Acquire.runengine import get_run_engine
 from xicam.Acquire.devices.diode import DetectorDiode
 from xicam.gui.widgets.imageviewmixins import CatalogImagePlotView
@@ -19,32 +22,60 @@ from xicam.plugins import manager as plugin_manager
 from xicam.core.msg import logError
 
 
-
 class DiodeController(ControllerPlugin):
 
     def __init__(self, device: Device):
         super(DiodeController, self).__init__(device)
 
         self.RE = get_run_engine()
-        self.plot_panel = CatalogImagePlotView(field_filter=None)
 
-        full_layout = QHBoxLayout()
+        full_layout = QVBoxLayout()
         self.setLayout(full_layout)
+
+        # create upper layout/widget
+        top_layout = QHBoxLayout()
+        top_widget = QWidget()
+        top_widget.setLayout(top_layout)
+        # add actual widgets to the top part
+        typhos_panel = TyphosCompositeSignalPanel.from_device(device)
+        typhos_panel.setMaximumHeight(50)
+        time_plot = PyDMTimePlot()
+        time_plot.setTimeSpan(120)
+        time_plot.addYChannel(y_channel=f'ca://{device.prefix}', name="Diode", color="red", lineWidth=3)
+        top_layout.addWidget(time_plot)
+        top_layout.addWidget(typhos_panel)
+
+        # create lower layout/widget
+        bottom_layout = QHBoxLayout()
+        bottom_widget = QWidget()
+        bottom_widget.setLayout(bottom_layout)
+        bottom_widget.setMinimumHeight(350)
+        # add actual widgets to the bottom part
+        self.plot_panel = CatalogImagePlotView(field_filter=None)
         config_layout = QVBoxLayout()
         scan_panel = QGroupBox('Diode Scan')
         scan_panel.setMaximumSize(200, 250)
         scan_panel.setLayout(config_layout)
-        full_layout.addWidget(self.plot_panel)
-        full_layout.addWidget(scan_panel)
+        bottom_layout.addWidget(self.plot_panel)
+        bottom_layout.addWidget(scan_panel)
+
+        # add a splitter
+        splitter = QSplitter(Qt.Vertical)
+        splitter.addWidget(top_widget)
+        splitter.addWidget(bottom_widget)
+
+        full_layout.addWidget(splitter)
 
         # get devices from happi database
         happi_settings = plugin_manager.get_plugin_by_name("happi_devices", "SettingsPlugin")
+
         def from_device_container(container) -> Device:
             try:
                 return from_container(container.device)
             except Exception as e:
                 logError(e)
                 return None
+
         self.async_poll_devices = list(map(from_device_container,
                                            happi_settings.search(source='labview',
                                                                  device_class='ophyd.EpicsMotor')
@@ -55,7 +86,7 @@ class DiodeController(ControllerPlugin):
         # Remove errored from_container devices (Nones)
         self.async_poll_devices = list(filter(lambda device: device, self.async_poll_devices))
         self.async_poll_devices.remove(device)
-        self.device_dict = {device.name : device for device in self.async_poll_devices}
+        self.device_dict = {device.name: device for device in self.async_poll_devices}
         # get the diode as detector
         diode = device.diode
 
@@ -81,7 +112,7 @@ class DiodeController(ControllerPlugin):
         start_button.clicked.connect(self.push_start)
         config_layout.addLayout(button_layout)
 
-        #TODO get data from bluesky run in callback
+        # TODO get data from bluesky run in callback
         # runs = []
         # self.RE.subscribe(stream_documents_into_runs(self.plot_panel.setCatalog))
         # self.plot_panel.setData(runs)
@@ -118,6 +149,7 @@ class RunDispatcher(CallbackBase):
 
 if __name__ == '__main__':
     import sys
+
     app = QApplication(sys.argv)
     w = DiodeController()
     w.show()
