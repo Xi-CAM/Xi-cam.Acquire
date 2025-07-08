8c59fa1d29da502e1b4a487bea5ef6b329fd79ea
gnzng <damian@guenzing.de>
2025-07-08 15:33:09 -0700

    Adding stop button to areadetector panel

--- xicam/Acquire/controllers/areadetector.py
+++ xicam/Acquire/controllers/areadetector.py
@@ -75,6 +75,10 @@ class AreaDetectorController(ControllerPlugin):
         acquire_button.clicked.connect(self.acquire)
         acquire_layout.addWidget(acquire_button)
 
+        self.stop_button = QPushButton('Stop')
+        self.stop_button.clicked.connect(self.stop)
+        acquire_layout.addWidget(self.stop_button)
+
         self.abort_button = QPushButton('Abort')
         self.abort_button.clicked.connect(self.abort)
         self._ready()  # prepare the appropriate abort btn check state and styling
@@ -93,6 +97,8 @@ class AreaDetectorController(ControllerPlugin):
         # Connect relevant RE signals to update abort btn check state and styling depending on the RE state
         self.RE.sigStart.connect(self._started)
         self.RE.sigReady.connect(self._ready)
+        self.RE.sigStop.connect(self._ready)
+        self.RE.sigAbort.connect(self._ready)
 
         # WIP
         # self.lutCheck = QCheckBox()
@@ -111,14 +117,21 @@ class AreaDetectorController(ControllerPlugin):
     def acquire(self):
         self.RE(count(self.coupled_devices), **self.metadata)
 
+    def stop(self):
+        self.RE.stop('Acquisition stopped by Xi-cam user.')
+
     def abort(self):
         self.RE.abort('Acquisition aborted by Xi-cam user.')
 
     def _started(self):
+        self.stop_button.setEnabled(True)
+        self.stop_button.setStyleSheet('background-color:orange;color:white;font-weight:bold;')
         self.abort_button.setEnabled(True)
         self.abort_button.setStyleSheet('background-color:red;color:white;font-weight:bold;')
 
     def _ready(self):
+        self.stop_button.setEnabled(False)
+        self.stop_button.setStyleSheet('')
         self.abort_button.setEnabled(False)
         self.abort_button.setStyleSheet('')
 
