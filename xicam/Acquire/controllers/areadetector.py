87077ca36a95dee94c94cb8537db9c905b75ce25
ronpandolfi <ronpandolfi@gmail.com>
2026-02-03 15:46:29 -0800

    remove log scale intensity

--- xicam/Acquire/controllers/areadetector.py
+++ xicam/Acquire/controllers/areadetector.py
@@ -24,7 +24,7 @@ class ADImageView(AreaDetectorROI,
                   PixelCoordinates,
                   Crosshair,
                   BetterButtons,
-                  LogScaleIntensity,
+                  # LogScaleIntensity,
                   ImageViewHistogramOverflowFix):
     pass
 
